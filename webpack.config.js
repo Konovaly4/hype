3aa80670bedb08f2ee0874822e6f0892100c1fd5
Maxim Konovalov <maxkonovalov82@yandex.ru>
2021-04-10 18:20:23 +0300

    webpack config modified & header is done

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   target: "web",
@@ -25,6 +26,10 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
@@ -44,5 +49,6 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html'
     }),
+    new CleanWebpackPlugin(),
   ]
 }
