33ac27c05a8d49552a48ee818925019c19fb6564
Maxim Konovalov <m.konovalov@offconoffice.local>
2021-04-21 18:05:23 +0300

    linters hooks added

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,35 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es6": true,
+        node: true,
     },
     "extends": [
-        "airbnb-base",
-        "prettier"
+        "airbnb-base"
+    ],
+    plugins: [
+        'babel',
+        'import',
+        'prettier',
     ],
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
     },
     "rules": {
+        'linebreak-style': 'off',
+        'arrow-parens': 'off',
+        'object-curly-newline': 'off',
+        'no-mixed-operators': 'off',
+        'arrow-body-style': 'off',
+        'function-paren-newline': 'off',
+        'no-plusplus': 'off',
+        'space-before-function-paren': 0,
+        'max-len': ['error', 100, 2, { ignoreUrls: true, }],
+        'no-console': 'error',
+        'no-alert': 'error',
+        'no-param-reassign': 'off',
+        'prefer-destructuring': 'off',
+        'prettier/prettier': ['error'],
     }
 };
