7a6b5a7659c372b0baeab235507b4d7c0afd3429
Max Konovalov <maxkonovalov82@yandex.ru>
2021-05-17 16:33:14 +0300

    temporary commit - slider width problem not fixed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import { tns } from '../node_modules/tiny-slider/src/tiny-slider';
 const slider = tns({
   container: '.my-slider',
   items: 1,
-  fixedWidth: false,
-  gutter: 50,
   slideBy: 'page',
-  autoplay: true,
   center: true,
-  controlsPosition: 'bottom',
-  swipeAngle: false,
 });
-slider.pause();
