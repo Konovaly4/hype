c1428dce3005a340d5c46587da2d8768b27b2ce2
Max Konovalov <maxkonovalov82@yandex.ru>
2021-05-28 15:43:26 +0300

    custom controls added and need to add CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import './style.scss';
 import { tns } from '../node_modules/tiny-slider/src/tiny-slider';
 
+const controlsContainer = document.querySelector('.slider__controls-container');
+const controlsPrevButton = document.querySelector('.slider__controls-button_prev');
+const controlsNextButton = document.querySelector('.slider__controls-button_next');
+const dotsContainer = document.querySelector('.slider__dots-container');
+
 const slider = tns({
   container: '.my-slider',
   items: 1,
   slideBy: 'page',
   center: true,
+  controlsContainer,
+  prevButton: controlsPrevButton,
+  nextButton: controlsNextButton,
+  navPosition: 'bottom',
+  navContainer: dotsContainer,
 });
