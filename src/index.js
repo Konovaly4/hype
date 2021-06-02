f39fd54639b84ac880ee70fa34224a82bb9f4f7c
Maxim Konovalov <maxkonovalov82@yandex.ru>
2021-06-02 23:29:58 +0300

    slider counter is in develop

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const controlsContainer = document.querySelector('.slider__controls-container');
 const controlsPrevButton = document.querySelector('.slider__controls-button_prev');
 const controlsNextButton = document.querySelector('.slider__controls-button_next');
 const dotsContainer = document.querySelector('.slider__dots-container');
+const sliderPicNumber = document.querySelector('.slider__pic-number');
+console.log(`pic - ${sliderPicNumber.textContent}`);
 
 const slider = tns({
   container: '.my-slider',
@@ -17,3 +19,35 @@ const slider = tns({
   navPosition: 'bottom',
   navContainer: dotsContainer,
 });
+
+sliderPicNumber.textContent = `${slider.getInfo().index}`;
+
+// tns-slide-active
+
+const showNextSlideIndex = (numberContainer) => {
+  const sliderItems = slider.getInfo().slideCount;
+  console.log(numberContainer);
+  const activeSlide = slider.getInfo().index;
+  if (activeSlide === sliderItems) {
+    numberContainer.textContent = '1';
+  } else {
+    numberContainer.textContent = `${activeSlide + 1}`;
+  }
+  console.log(numberContainer.textContent);
+};
+
+const showPrevSlideIndex = (numberContainer) => {
+  const sliderItems = slider.getInfo().slideCount;
+  console.log(numberContainer);
+  const activeSlide = slider.getInfo().index;
+  if (activeSlide === 1) {
+    numberContainer.textContent = `${sliderItems}`;
+  } else {
+    numberContainer.textContent = `${activeSlide - 1}`;
+  }
+  console.log(numberContainer.textContent);
+};
+
+controlsPrevButton.addEventListener('click', showPrevSlideIndex(sliderPicNumber));
+
+controlsNextButton.addEventListener('click', showNextSlideIndex(sliderPicNumber));
